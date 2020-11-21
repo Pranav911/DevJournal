e67d188f9ac09b779ffd79936682c765049cc661
Pranav Jain <pranavjain2020@gmail.com>
2020-11-21 19:36:07 +0530

    Complexities added

--- src/com/devjournal/datastructures/ArrayImpl.java
+++ src/com/devjournal/datastructures/ArrayImpl.java
@@ -1,38 +1,38 @@
 package com.devjournal.datastructures;
 
 public class ArrayImpl {
-	// 1,2,3,4,5, 
-	// Insertion -> end, begin, pos <-Deletion
-	
 	private int capacity = 5;
 	private int[] arr = new int[capacity];
 	private int size = -1;
 	
+	// O(n)
 	public void insertAtBegin(int element) {
-		if(size+1==capacity) {
+		if(size+1 == capacity) {
 			resize();
 		}
-		for(int i=size; i>=0; i--) {
-			arr[i+1] =arr[i];
+		for(int i=size; i >= 0; i--) {
+			arr[i+1] = arr[i];
 		}
 		arr[0] = element;
 		size++;
 	}
 	
+	// O(1)
 	public void insertAtEnd(int element) {
-		if(size+1==capacity) {
+		if(size+1 == capacity) {
 			resize();
 		}
 		arr[size+1] = element;
 		size++;
 	}
 	
+	// O(n)
 	public void insertAtPos(int element, int pos) {
-		if(pos<1 || pos>size+2) {
+		if(pos < 1 || pos > size+2) {
 			System.out.println("Invalid position");
 			return;
 		}
-		if(size+1==capacity) {
+		if(size+1 == capacity) {
 			resize();
 		}
 		for(int i=size; i>=pos-1; i--) {
@@ -43,8 +43,9 @@ public class ArrayImpl {
 		
 	}
 	
+	// O(n)
 	public void deleteAtBegin() {
-		if(size==-1) {
+		if(size == -1) {
 			System.out.println("underflow");
 			return;
 		}
@@ -54,6 +55,7 @@ public class ArrayImpl {
 		size--;
 	}
 	
+	// O(1)
 	public void deleteAtEnd() {
 		if(size==-1) {
 			System.out.println("Underflow");
@@ -62,12 +64,13 @@ public class ArrayImpl {
 		size--;
 	}
 	
+	// O(n)
 	public void deleteAtPos(int pos) {
 		if(size==-1) {
 			System.out.println("Underflow");
 			return;
 		}
-		if(pos<1 || pos>size+1) {
+		if(pos < 1 || pos > size+1) {
 			System.out.println("Invalid position for deletion");
 			return;
 		}
@@ -78,6 +81,7 @@ public class ArrayImpl {
 		
 	}
 
+	// O(n)
 	public void resize() {
 		int[] tempArr = new int[capacity*2];
 		for(int i=0; i<capacity; i++) {
@@ -87,6 +91,7 @@ public class ArrayImpl {
 		capacity *= 2;
 	}
 	
+	// O(n)
 	public void print() {
 		for(int i=0; i<=size; i++) {
 			System.out.print(arr[i] + " ");
@@ -104,6 +109,8 @@ public class ArrayImpl {
 	
 	public static void main(String[] args) {
 		ArrayImpl ob = new ArrayImpl();
+		// 1 2 3 4 5 6
+		// 2 4 5
 		ob.insertAtBegin(3);
 		ob.insertAtBegin(2);
 		ob.insertAtBegin(1);
